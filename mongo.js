71593335d2981073d94c690c4d80565de6a969cf
keinokalle <keinokalle@gmail.com>
2025-06-19 10:28:27 +0300

    the changes before moving to exercise 3.13

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const newName = process.argv[3]
+const newNumber = process.argv[4]
+
+const url = `mongodb+srv://phoneBookKalle:${password}@phonebook.gb2m3mp.mongodb.net/personData?retryWrites=true&w=majority&appName=phoneBook`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(newName!==undefined && newNumber!==undefined) {
+    const person = new Person({
+    name: newName,
+    number: newNumber,
+    })
+
+    person.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+    })
+
+} else {
+    Person.find({}).then(result => {
+    result.forEach(p => {
+        console.log(p)
+    })
+    mongoose.connection.close()
+    })
+}
+
