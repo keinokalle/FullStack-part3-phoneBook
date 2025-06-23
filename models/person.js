84e3ebb4e38a69b53a22a84b392091fd67f901a9
keinokalle <keinokalle@gmail.com>
2025-06-23 16:25:59 +0300

    starting the round 3.4

--- models/person.js
+++ models/person.js
@@ -17,8 +17,12 @@ mongoose.connect(url)
 
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: String
 })
 
 
